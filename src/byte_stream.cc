3de29ab9e7b40d0af521930eacc18e4ae5e5a92e
koarz <3577087577@qq.com>
2024-09-06 23:45:55 +0800

    remove useless code

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,6 +1,4 @@
 #include "byte_stream.hh"
-#include <iostream>
-#include <iterator>
 #include <string_view>
 
 using namespace std;
@@ -9,16 +7,14 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
   return closed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  if ( data_.size() == capacity_ )
+  if ( data_.size() == capacity_ ) {
     return;
-  else if ( data.size() + data_.size() <= capacity_ ) {
+  } else if ( data.size() + data_.size() <= capacity_ ) {
     data_ += data;
     pushed_ += data.size();
   } else {
@@ -30,43 +26,36 @@ void Writer::push( string data )
 
 void Writer::close()
 {
-  // Your code here.
   closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
   return capacity_ - data_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
   return pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
   return closed_ && data_.size() == 0;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
   return poped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
   return data_;
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
   if ( len >= data_.size() ) {
     poped_ += data_.size();
     data_.clear();
@@ -78,6 +67,5 @@ void Reader::pop( uint64_t len )
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
   return data_.size();
 }
