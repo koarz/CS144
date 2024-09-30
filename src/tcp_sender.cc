93a8f7f0bfdc8d687b9b164edbc120dc0c51f0f0
koarz <3577087577@qq.com>
2024-09-30 18:38:06 +0800

    reduce the amount of code

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -25,22 +25,6 @@ void TCPSender::push( const TransmitFunction& transmit )
     return;
   }
 
-  if ( input_.reader().bytes_buffered() == 0 ) {
-    // transmit( make_empty_message() );
-    if ( !send_fin_ && ( ( window_size_ == 1 && first_receive_ ) || input_.writer().is_closed() ) ) {
-      auto mess = make_empty_message();
-      mess.FIN = true;
-      send_fin_ = true;
-      data_slice_.emplace( mess.seqno, mess );
-      data_key_.push_back( mess.seqno );
-      next_seqno_ += mess.sequence_length();
-      number_in_flight_ += mess.sequence_length();
-      window_size_--;
-      transmit( mess );
-    }
-    return;
-  }
-
   init_data_slice_and_push( transmit );
 }
 
@@ -54,18 +38,12 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
   if ( msg.RST ) {
     input_.set_error();
   }
-  if ( !msg.ackno.has_value() ) {
-    return;
-  }
-  if ( msg.ackno.value() < isn_ + input_.reader().bytes_popped() + 1
+  if ( !msg.ackno.has_value() || msg.ackno.value() < isn_ + input_.reader().bytes_popped() + 1
        || msg.ackno.value().unwrap( isn_, next_seqno_ ) > next_seqno_ ) {
     return;
   }
   if ( !first_receive_ ) {
     first_receive_ = true;
-    number_in_flight_--;
-    data_slice_.erase( isn_ );
-    data_key_.pop_front();
   }
   window_size_ = *reinterpret_cast<const uint32_t*>( &msg.ackno.value() ) + msg.window_size
                  - ( *reinterpret_cast<uint32_t*>( &isn_ ) + next_seqno_ );
@@ -110,62 +88,26 @@ void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& trans
 void TCPSender::init_data_slice_and_push( const TransmitFunction& transmit )
 {
   uint64_t pos { number_in_flight_ };
-  if ( !send_fin_ && input_.writer().is_closed()
-       && ( window_size_ > input_.reader().bytes_buffered() - number_in_flight_
-            || ( window_size_ == 0 && number_in_flight_ == 0 ) ) ) {
-    auto mess = make_empty_message();
-    mess.FIN = true;
-    send_fin_ = true;
-    if ( window_size_ > 1 ) {
-      auto data_size = std::min( { input_.reader().bytes_buffered() - pos,
-                                   TCPConfig::MAX_PAYLOAD_SIZE,
-                                   static_cast<uint64_t>( window_size_ ) } );
-      if ( data_size == 0 ) {
-        goto FLAG;
-      }
-      if ( pos + data_size < input_.reader().bytes_buffered() ) {
-        goto FLAG2;
-      }
-      mess.payload = std::string( input_.reader().peek().substr( pos, data_size ) );
-      if ( data_size >= window_size_ ) {
-        mess.FIN = false;
-        send_fin_ = false;
-      }
-    }
-  FLAG:
-    data_key_.push_back( mess.seqno );
-    data_slice_.emplace( mess.seqno, mess );
-    number_in_flight_ += mess.sequence_length();
-    next_seqno_ += mess.sequence_length();
-    if ( window_size_ > 0 )
-      window_size_ -= mess.sequence_length();
-    transmit( mess );
-    return;
-  }
-FLAG2:
-  while ( true ) {
+  while ( !send_fin_ ) {
     auto data_size = std::min( { input_.reader().bytes_buffered() - pos,
                                  TCPConfig::MAX_PAYLOAD_SIZE,
                                  static_cast<uint64_t>( window_size_ ) } );
-    if ( data_size == 0 ) {
+    send_fin_ = !send_fin_ && input_.writer().is_closed()
+                && ( window_size_ > input_.reader().bytes_buffered() - number_in_flight_ )
+                && !( pos + data_size < input_.reader().bytes_buffered() );
+    if ( data_size == 0 && !send_fin_ ) {
       return;
     }
-    if ( pos >= input_.reader().bytes_buffered() ) {
+    if ( pos > input_.reader().bytes_buffered() ) {
       return;
     }
     auto seqno = isn_ + next_seqno_;
-    auto mess = TCPSenderMessage { seqno,
-                                   false,
-                                   std::string( input_.reader().peek().substr( pos, data_size ) ),
-                                   input_.writer().is_closed()
-                                     && ( window_size_ > input_.reader().bytes_buffered() - number_in_flight_ )
-                                     && !( pos + data_size < input_.reader().bytes_buffered() ),
-                                   input_.has_error() };
+    auto mess = TCPSenderMessage {
+      seqno, false, std::string( input_.reader().peek().substr( pos, data_size ) ), send_fin_, input_.has_error() };
     pos += mess.sequence_length();
     next_seqno_ += mess.sequence_length();
     number_in_flight_ += mess.sequence_length();
     window_size_ -= mess.sequence_length();
-
     transmit( mess );
     data_key_.push_back( seqno );
     data_slice_.emplace( std::move( seqno ), std::move( mess ) );
